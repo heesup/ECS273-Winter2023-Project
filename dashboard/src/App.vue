ce736df51d8b3bb818bc6d4e89a6cb7d1da2f082
Heesup Yun <hspyun@ucdavis.edu>
2023-03-01 18:03:26 -0800

    Add Draft View

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,15 +1,16 @@
 <script lang="ts">
-import Notes from './components/Notes.vue'
-import Example from './components/Example.vue'
-import ExampleWithLegend from './components/ExampleWithLegend.vue';
-import ExampleWithInteractions from './components/ExampleWithInteractions.vue';
+import ScatterPlotView from './components/ScatterPlotView.vue';
+import BarPlotView from './components/BarPlotView.vue';
+import ParallelPlotView from './components/ParallelPlotView.vue';
+import SimulationView from './components/SimulationView.vue';
+
 
 export default {
   components: {
-    Notes,
-    Example,
-    ExampleWithLegend,
-    ExampleWithInteractions
+    ScatterPlotView,
+    BarPlotView,
+    ParallelPlotView,
+    SimulationView,
   }
 }
 </script>
@@ -17,16 +18,43 @@ export default {
 <!--This is using the grid component from Vuetify to do layout design-->
 <template>
   <v-container id="main-container" class="d-flex flex-column flex-nowrap" fluid>
-    <v-row no-gutters>
-      <v-col cols="5">
-        <ExampleWithLegend/>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col>
-        <Notes msg="This message is showcasing how to pass down the props"/>
-      </v-col>
-    </v-row>
+  <v-row>
+    <v-col cols="7">
+      <v-row no-gutters>
+        <v-col cols="7">
+          <!-- <ScatterPlotView/> -->
+          <img src="./components/ScatterPlotView.png" height="300">
+        </v-col>
+        
+        <v-col cols="3">
+          <!-- <BarPlotView/> -->
+          <img src="./components/BarPlotView.png" height="300">
+        </v-col>
+      </v-row>
+      
+      <v-row no-gutters>
+        <v-col>
+          <!-- <Notes msg="This message is showcasing how to pass down the props"/> -->
+          <!-- <ParallelPlotView/> -->
+          <img src="./components/ParallelPlotView.png" width="730">
+        </v-col>
+      </v-row>
+    </v-col>
+    
+    <v-col cols="3">
+      <v-row no-gutters>
+        <v-col>
+          <!-- <SimulationView/> -->
+          <img src="./components/SimulationView.png" height="600">
+        </v-col>
+      </v-row>
+    </v-col>
+
+  </v-row>
+  
+
+
+
   </v-container>
 </template>
 
