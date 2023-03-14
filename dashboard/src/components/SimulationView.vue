ead47eb6a525ae4efd68c08b8de86db625b432b9
Heesup Yun <hspyun@ucdavis.edu>
2023-03-14 15:05:31 -0700

    Added radio and sliders to Simulator

--- dashboard/src/components/SimulationView.vue
+++ dashboard/src/components/SimulationView.vue
@@ -21,6 +21,10 @@ export default {
             clusters: [] as string[],
             size: { width: 0, height: 0 } as ComponentSize,
             margin: {left: 20, right: 20, top: 20, bottom: 40} as Margin,
+            MFGs: ['Seagate', 'TOSHIBA', 'HGST', 'WDC', 'Micron', 'HP', 'Hitachi', 'DELLBOSS'] as string[],
+            selectedMFG: "Seagate" as string,
+            selectedCapacity: 0 as number,
+            useMonth:0,
         }
     },
     computed: {
@@ -74,11 +78,70 @@ export default {
 <!-- "ref" registers a reference to the HTML element so that we can access it via the reference in Vue.  -->
 <!-- We use flex to arrange the layout-->
 <template>
-    <div class="chart-container d-flex" ref="scatterContainer">
+    <div>
+        <p style="text-align:center;font-size:20px">HDD Failure Simulator</p>
+    </div>
+
+    <div class="radioMFG" style="">
+
+        <input type="radio" name="" id="Seagate" value="Seagate" v-model="selectedMFG">
+        <label for="Seagate">Seagate</label>
+
+        <input type="radio" name="" id="TOSHIBA" value="TOSHIBA" v-model="selectedMFG">
+        <label for="TOSHIBA">TOSHIBA</label>
+      
+        <input type="radio" name="" id="HGST" value="HGST" v-model="selectedMFG">
+        <label for="HGST">HGST</label>
+
+        <input type="radio" name="" id="WDC" value="WDC" v-model="selectedMFG">
+        <label for="WDC">WDC</label>
+
+        <input type="radio" name="" id="Micron" value="Micron" v-model="selectedMFG">
+        <label for="Micron">Micron</label>
+
+        <input type="radio" name="" id="HP" value="HP" v-model="selectedMFG">
+        <label for="HP">HP</label>
+
+        <input type="radio" name="" id="Hitachi" value="Hitachi" v-model="selectedMFG">
+        <label for="Hitachi">Hitachi</label>
+
+        <input type="radio" name="" id="DELLBOSS" value="DELLBOSS" v-model="selectedMFG">
+        <label for="DELLBOSS">DELLBOSS</label>
+    </div>
+
+    <div style="text-align:center;font-size: 15px;">
+        Capacity (TB)
+        <select name="" v-model="selectedCapacity">
+            <!-- Todo: Add MFG and Capacity Combination -->
+            <option value=2>2TB</option>
+            <option value=4>4TB</option>
+            <option value=6>6TB</option>
+            <option value=8>8TB</option>
+            <option value=10>10TB</option>
+            <option value=12>12TB</option>
+            <option value=14>14TB</option>
+            <option value=16>16TB</option>
+        </select>
+    </div>
+
+    <v-app>
+        <div>
+            <!-- <v-slider label="Capacity" v-model="slider" :value="slider" track-color="grey" always-dirty min="1" max="36" thumb-label="always"/> -->
+            <v-slider label="Capacity" v-model="selectedCapacity" :value="selectedCapacity" track-color="grey" always-dirty min="1" max="36" thumb-label/>
+            <v-slider label="Month" v-model="useMonth" :value="useMonth" track-color="grey" always-dirty min="1" max="36" thumb-label/>
+            <!-- 임시로 -->
+            
+        </div>
+        <div class="chart-container d-flex" ref="scatterContainer"> 
+            <img src="src/components/Screenshot 2023-03-14 at 1.37.43 PM.png" width="475">
         <svg id="simulation-svg" width="100%" height="100%">
             <!-- all the visual elements we create in initChart() will be inserted here in DOM-->
         </svg>
-    </div>
+        </div>
+    </v-app>
+    
+
+
 </template>
 
 <style scoped>
@@ -88,5 +151,9 @@ export default {
     /* for debug */
     border: 1px;
     border-style: dashed;
+}
+.radioMFG{
+    font-size: 15px;
+    text-align:center;
 }
 </style>
